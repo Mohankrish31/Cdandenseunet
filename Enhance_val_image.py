555c6de0661af9e10a7cd8e6336be5a815d47a0d
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-08-20 19:01:57 +0530

    Update Enhance_val_image.py

--- Enhance_val_image.py
+++ Enhance_val_image.py
@@ -23,7 +23,8 @@ model.eval()
 # -------- Preprocessing --------
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 ])
 # -------- Enhance and save images --------
 with torch.no_grad():
