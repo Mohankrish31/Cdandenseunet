0dd3bf521f97c693fe97b5bf15095feb69555a8c
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-08-21 08:39:40 +0530

    Update Enhance_val_image.py

--- Enhance_val_image.py
+++ Enhance_val_image.py
@@ -10,7 +10,7 @@ sys.path.append('/content/Cdandenseunet')
 from models.cdan_denseunet import CDANDenseUNet
 # -------- Paths --------
 input_dir = "/content/cvccolondbsplit/val/low"   # Low-light train images
-output_dir = "/content/drive/MyDrive/Colon_Enhanced/val_enhanced"
+output_dir = "/content/outputs/val_enhanced"
 model_path = "/content/saved_model/cdan_denseunet.pt"
 # -------- Create output directory --------
 os.makedirs(output_dir, exist_ok=True)
