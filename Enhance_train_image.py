b387281c049b2323909435f43770dd8e32a1d728
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-08-25 10:01:39 +0530

    Update Enhance_train_image.py

--- Enhance_train_image.py
+++ Enhance_train_image.py
@@ -8,8 +8,8 @@ from torchvision.transforms.functional import to_pil_image
 sys.path.append('/content/CdanDenseUNet')  # Adjust to your folder
 from models.cdan_denseunet import CDANDenseUNet
 # ------------------- Paths -------------------
-input_dir = "/content/cvccolondbsplit/test/low"       # Low-light images
-output_dir = "/content/outputs/test_enhanced"        # Enhanced images save path
+input_dir = "/content/cvccolondbsplit/train/low"       # Low-light images
+output_dir = "/content/outputs/train_enhanced"        # Enhanced images save path
 model_path = "/content/saved_model/cdan_denseunet_weights.pth"  # Trained weights
 os.makedirs(output_dir, exist_ok=True)
 # ------------------- Device -------------------
