66030f8d13d318c702b8cab6f35d76f91cce5da4
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-08-25 11:02:56 +0530

    Update Enhance_train_image.py

--- Enhance_train_image.py
+++ Enhance_train_image.py
@@ -10,7 +10,7 @@ from models.cdan_denseunet import CDANDenseUNet
 # ------------------- Paths -------------------
 input_dir = "/content/cvccolondbsplit/train/low"       # Low-light images
 output_dir = "/content/outputs/train_enhanced"        # Enhanced images save path
-model_path = "/content/saved_model/cdan_denseunet_weights.pth"  # Trained weights
+model_path = "/content/saved_model/cdan_denseunet_isp_weights.pth"  # Trained weights
 os.makedirs(output_dir, exist_ok=True)
 # ------------------- Device -------------------
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
