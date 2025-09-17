9c70edfb1521f8123b8e435d43722456dce6dcd9
Mohankrish31 <134041145+Mohankrish31@users.noreply.github.com>
2025-09-17 15:11:20 +0530

    Update dataloader.py

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -4,7 +4,7 @@ import os
 from PIL import Image
 from torchvision import transforms
 # === Dataset ===
-class cvccolondbDataset(Dataset):
+class cvccolondbsplitDataset(Dataset):
     def __init__(self, enhanced_dir, high_dir, transform=None):
         self.enhanced_dir = enhanced_dir
         self.high_dir = high_dir
@@ -24,7 +24,7 @@ class cvccolondbDataset(Dataset):
         if self.transform:
             enhanced_img = self.transform(enhanced_img)
             high_img = self.transform(high_img)
-        return enhanced_img, high_img
+        return high_img,enhanced_img 
 # === Define Transforms ===
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -36,8 +36,8 @@ train_high_dir = "/content/cvccolondbsplit/train/high"
 val_enhanced_dir = "/content/drive/MyDrive/Colon_Enhanced/val_enhanced"
 val_high_dir = "/content/cvccolondbsplit/val/high"
 # === Create Dataset Instances ===
-train_dataset = cvccolondbDataset(train_enhanced_dir, train_high_dir, transform=transform)
-val_dataset = cvccolondbDataset(val_enhanced_dir, val_high_dir, transform=transform)
+train_dataset = cvccolondbsplitDataset(train_enhanced_dir, train_high_dir, transform=transform)
+val_dataset = cvccolondbsplitDataset(val_enhanced_dir, val_high_dir, transform=transform)
 # === Create DataLoaders ===
 batch_size = 8
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
